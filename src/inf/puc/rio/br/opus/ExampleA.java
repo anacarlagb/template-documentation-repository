10a47a1e362b2c9d56935125386db47e4f843fbe
anacarlagb <ana.carlagb@gmail.com>
2019-10-19 22:36:59 -0300

    Starting repository

---
+++ src/inf/puc/rio/br/opus/ExampleA.java
@@ -0,0 +1,16 @@
+package inf.puc.rio.br.opus;
+
+/**
+ * @author Your Name
+ * @class: ExampleA - responsible by ...
+ **/
+public class ExampleA {
+
+	
+	/**
+	 * @method: methodA - responsible by ...
+	 * */
+	public void methodA(){
+		
+	}
+}
